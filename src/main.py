85270a53f5534997c49e70992ee73c809b99b7b5
Youzhi Liu <youzhili@usc.edu>
2023-11-15 17:02:03 -0800

    Finished data preprocessing
    Took so much time for the huge dataset, but works for the smaller dataset

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sentiment_analyzer import analyze_sentiment, calculate_z_scores
 from data_processing import load_datasets, preprocess_text
-from sentiment_analyzer import analyze_sentiment, aggregate_sentiment
 
 def main():
     # Load and merge data
     final_df = load_datasets()
 
-    # Preprocess tweets
+    # Data Preprocessing
     final_df['cleaned_tweet'] = final_df['body'].apply(preprocess_text)
-
-    # Perform sentiment analysis
     final_df['sentiment'] = final_df['cleaned_tweet'].apply(analyze_sentiment)
+    final_df = calculate_z_scores(final_df)
 
-    # Calculate the average sentiment for each company
-    aggregated_sentiment = aggregate_sentiment(final_df, 'company_name', 'sentiment')
+    # Vectorization with limited features and using sparse matrix
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2), max_features=10000)
+    X = vectorizer.fit_transform(final_df['cleaned_tweet'])
 
-    print(aggregated_sentiment)
+    # Create labels based on the sentiments
+    final_df['label'] = final_df['z_score'].apply(lambda x: 1 if x > 0.5 else -1)
+    y = final_df['label']
+    # print(final_df)
 
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Initialize and train the RandomForest classifier
+    clf = RandomForestClassifier(random_state=42)
+    clf.fit(X_train, y_train)
+
+    # Predict and evaluate the classifier
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_test, y_pred))
 if __name__ == "__main__":
     main()
-
-
--- src/sentiment_analyzer.py
+++ src/sentiment_analyzer.py
@@ -1,4 +1,6 @@
 from textblob import TextBlob
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import pandas as pd
 def analyze_sentiment(tweet):
     analysis = TextBlob(tweet)
     return analysis.sentiment.polarity
@@ -6,3 +8,26 @@ def analyze_sentiment(tweet):
 def aggregate_sentiment(dataframe, group_by_column, sentiment_column):
     aggregated_data = dataframe.groupby(group_by_column)[sentiment_column].mean().reset_index()
     return aggregated_data
+def calculate_z_scores(dataframe, window=15):
+    # Calculate the rolling mean and standard deviation
+    rolling_mean = dataframe['sentiment'].rolling(window=window).mean()
+    rolling_std = dataframe['sentiment'].rolling(window=window).std()
+
+    # Calculate Z-scores
+    dataframe['z_score'] = (dataframe['sentiment'] - rolling_mean) / rolling_std
+
+    # Drop the NaN values that result from rolling function
+    dataframe = dataframe.dropna(subset=['z_score'])
+
+    return dataframe
+def vec_fun(data, m, n, max_features=10000, method='tf-idf'):
+    if method == 'cv':
+        vectorizer = CountVectorizer(ngram_range=(m, n), max_features=max_features)
+    elif method == 'tf-idf':
+        vectorizer = TfidfVectorizer(ngram_range=(m, n), max_features=max_features)
+    else:
+        raise ValueError("Invalid method specified. Choose 'cv' or 'tf-idf'.")
+
+    xform_data = vectorizer.fit_transform(data)
+    return xform_data, vectorizer.get_feature_names_out()
+
