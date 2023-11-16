2ce6f9b19cbc9dd433e69e43df89cdcf88909419
Bryyyyyyan <150535600+Bryyyyyyan@users.noreply.github.com>
2023-11-16 03:53:53 -0800

    Add files via upload

---
+++ src/Linear_Regression_Model.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_squared_error, r2_score
+
+def linear_regression_model(company_df):
+    X = company_df['z_score']
+    y = company_df['Daily Difference']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
